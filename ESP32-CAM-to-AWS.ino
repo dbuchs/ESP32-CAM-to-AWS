0d9897abee1c786681589ca8403088f1fbf0c4a2
dbuchs <david.buchs@gmail.com>
2023-10-25 08:53:52 -0500

    Initializing files, headers

---
+++ ESP32-CAM-to-AWS.ino
@@ -0,0 +1,20 @@
+
+// AWS Certificates, etc.
+#include "secrets.h"
+
+// API for sending MQTT message to AWS IoT Core
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+
+#include "WiFi.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
