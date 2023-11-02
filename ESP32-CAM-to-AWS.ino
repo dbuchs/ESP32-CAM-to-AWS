a73d70751b9f84f38b9db12dec70a604f0d1dd29
dbuchs <david.buchs@gmail.com>
2023-11-01 21:14:04 -0500

    Branch that attempts to use IOT, file size limits

--- ESP32-CAM-to-AWS.ino
+++ ESP32-CAM-to-AWS.ino
@@ -1,7 +1,5 @@
-
 // AWS Certificates, etc.
 #include "secrets.h"
-#include "Base64.h"
 
 // API for sending MQTT message to AWS IoT Core
 #include <WiFiClientSecure.h>
@@ -10,29 +8,11 @@
 #include "WiFi.h"
 #include "esp_camera.h"
 
+#include "HTTPClient.h"
 
 // The MQTT topics that this device should publish/subscribe
 #define AWS_IOT_PUBLISH_TOPIC "esp32/pub"
-#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/pub"
-
-// Camera Model
-// CAMERA_MODEL_AI_THINKER
-#define PWDN_GPIO_NUM 32
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 0
-#define SIOD_GPIO_NUM 26
-#define SIOC_GPIO_NUM 27
-#define Y9_GPIO_NUM 35
-#define Y8_GPIO_NUM 34
-#define Y7_GPIO_NUM 39
-#define Y6_GPIO_NUM 36
-#define Y5_GPIO_NUM 21
-#define Y4_GPIO_NUM 19
-#define Y3_GPIO_NUM 18
-#define Y2_GPIO_NUM 5
-#define VSYNC_GPIO_NUM 25
-#define HREF_GPIO_NUM 23
-#define PCLK_GPIO_NUM 22
+#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
 
 WiFiClientSecure net = WiFiClientSecure();
 PubSubClient client(net);
@@ -59,7 +39,7 @@ void connectAWS() {
   client.setServer(AWS_IOT_ENDPOINT, 8883);
   Serial.println("Began client");
   client.setKeepAlive(30);
-  //Serial.println("Setting Buffer Size to 40032: " + String(client.setBufferSize(40032)));
+  Serial.println("Setting Buffer Size to 40032: " + String(client.setBufferSize(40032)));
 
   Serial.println("Connecting to AWS IOT");
 
@@ -141,9 +121,16 @@ void publishMessage() {
     base64_encode(output, (input++), 3);
     if (i % 3 == 0) image += String(output);
   }
+  esp_camera_fb_return(fb);
 
-  String imageFile = "{\"image\":\"" + image + "\"}";
-  uint32_t fbLen = imageFile.length();
+  char timestamp[12];
+  itoa(millis(), timestamp, 10);
+  char jsonStart[30] = "{\"file\":\"";
+  strcat(jsonStart, timestamp);
+  strcat(jsonStart, "\",\"image\":\"");
+  Serial.println(jsonStart);
+  char jsonEnd[] = "\"}";
+  Serial.println(jsonEnd);
 
   if (!client.connected()) {
     Serial.println("Connecting to AWS IOT");
@@ -159,108 +146,48 @@ void publishMessage() {
     }
   }
 
-  boolean res = client.beginPublish(AWS_IOT_PUBLISH_TOPIC, fbLen, false);
-  Serial.println("publish began " + String(res));
 
-  for (uint32_t n = 0; n < fbLen; n = n + 2048) {
-    String str = "";
-    if (n + 2048 < fbLen) {
-      str = imageFile.substring(n, n + 2048);
-      client.write((uint8_t *)str.c_str(), 2048);
-    } else if (fbLen % 2048 > 0) {
-      uint32_t remainder = fbLen % 2048;
-      str = imageFile.substring(n, n + remainder);
-      client.write((uint8_t *)str.c_str(), remainder);
+
+  uint32_t fbLen = image.length();
+  String str = "";
+  String imageFile = "";
+  String chunk = "";
+  for (uint32_t n = 0; n < fbLen; n = n + 40000) {
+
+    if (n + 40000 < fbLen) {
+      str = image.substring(n, n + 40000);
+    } else if (fbLen % 40000 > 0) {
+      uint32_t remain = fbLen % 40000;
+      str = image.substring(n, n + remain);
     }
+    imageFile = String(jsonStart) + str + String(jsonEnd);
+    //Serial.println(String(imageFile.length()));
+    //Serial.println(imageFile.substring(0,40));
+    //Serial.println(imageFile.substring(imageFile.length()-40,imageFile.length()));
+    //Serial.println(client.publish(AWS_IOT_PUBLISH_TOPIC, (uint8_t*)imageFile.c_str(),imageFile.length(), false));
+    size_t chunkLen = imageFile.length();
+    boolean res = client.beginPublish(AWS_IOT_PUBLISH_TOPIC, chunkLen, false);
+    Serial.println("publish began " + String(res));
+
+    for (size_t z = 0; z < chunkLen; z = z + 2048) {
+      if (z + 2048 < chunkLen) {
+        chunk = imageFile.substring(z, z + 2048);
+        client.write((uint8_t *)chunk.c_str(), 2048);
+      } else if (chunkLen % 2048 > 0) {
+        size_t remainder = chunkLen % 2048;
+        chunk = imageFile.substring(z, z + remainder);
+        client.write((uint8_t *)chunk.c_str(), remainder);
+      }
+      Serial.println(String(z));
+    }
+
+    Serial.println("publish written " + String(res));
+
+    res = client.endPublish();
+    Serial.println("publish ended " + String(res));
     Serial.println(String(n));
   }
-
-  Serial.println("publish written " + String(res));
-
-  res = client.endPublish();
-  Serial.println("publish ended " + String(res));
-
-  esp_camera_fb_return(fb);
 }
-
-// void publishMessage() {
-// Serial.println("in publish messae");
-// camera_fb_t* fb = NULL;
-// fb = esp_camera_fb_get();
-// if (!fb) {
-//   Serial.println("Camera capture failed");
-//   return;
-// }
-
-// char* input = (char*)fb->buf;
-
-// //AWS Lambda Actions need base64 encoded data, jsonified
-// char output[base64_enc_len(3)];
-// char* image = (char*)ps_malloc(500000);
-// char* image_ptr = image;  // Create a pointer to keep track of the end of the 'image' buffer
-
-// Serial.println("allocated memory");
-
-// for (int i = 0; i < fb->len; i++) {
-//   base64_encode(output, input, 3);
-//   strcpy(image_ptr, output);
-//   image_ptr += strlen(output);
-//   input += 3;
-// }
-// Serial.println("encloded");
-// uint32_t fbLen = strlen(image);
-
-
-// if (!client.connected()) {
-//   Serial.println("Connecting to AWS IOT");
-
-//   while (!client.connect(THINGNAME)) {
-//     Serial.print(".");
-//     delay(100);
-//   }
-
-//   if (!client.connected()) {
-//     Serial.println("AWS IoT Timeout!");
-//     return;
-//   }
-// }
-
-// //boolean res = client.beginPublish(AWS_IOT_PUBLISH_TOPIC, fbLen+12, false);
-// //Serial.println("publish began " + String(res));
-
-// char timestamp[12];
-// itoa(millis(), timestamp, 10);
-// char jsonStart[30] = "{\"file\":\"";
-// strcat(jsonStart, timestamp);
-// strcat(jsonStart, "\",\"image\":\"");
-// Serial.println(jsonStart);
-// char jsonEnd[] = "\"}";
-// Serial.println(jsonEnd);
-
-// //client.write((uint8_t*)"{\"image\":\"",10);
-
-// for (uint32_t n = 0; n < fbLen; n = n + 40000) {
-//   char buffer[40032];
-//   strcpy(buffer, jsonStart);
-//   if (n + 40000 < fbLen) {
-//     strncat(buffer, image + n, 40000);
-//   } else if (fbLen % 40000 > 0) {
-//     size_t remainder = fbLen % 40000;
-//     strncpy(buffer + 32, image + n, remainder);
-//   }
-//   strcat(buffer, jsonEnd);
-//   Serial.println("published " + client.publish(AWS_IOT_PUBLISH_TOPIC, buffer));
-//   Serial.println(String(n));
-// }
-
-// // Serial.println("publish written " + String(res));
-
-// // res = client.endPublish();
-// // Serial.println("publish ended " + String(res));
-
-//esp_camera_fb_return(fb);
-// free(image);
-//}
 
 
 void setup() {
@@ -276,6 +203,6 @@ void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("looping now");
   publishMessage();
-  delay(10000);
+  delay(30000);
   client.loop();
 }
